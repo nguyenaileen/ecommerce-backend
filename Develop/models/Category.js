6526fd3224ddc05440b4d28509f2d5be8b3a52fa
Aileen Nguyen <aileenn@gmail.com>
2024-06-10 20:44:51 -0700

    set up category columns in models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -4,9 +4,20 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+//create columns
 Category.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.Integer, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true, 
+    }
+
+   category _name:{
+    type:DataTypes.string,
+    allowNull: false, 
+   }
   },
   {
     sequelize,
