f00f43e00a3bfe0d8dc16f504475e80a70a6e2cb
Aileen Nguyen <aileenn@gmail.com>
2024-06-10 20:58:51 -0700

    set up models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,34 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Products belongsTo
+
+Product.belongsTo(Category, {
+  foreignKay: "product_id",
+});
 
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKay: "category_id",
+});
+
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag, // The intermediate model
+  foreignKey: "product_id", // The foreign key in the Product model
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
